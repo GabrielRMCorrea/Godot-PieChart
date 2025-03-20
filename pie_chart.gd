42a04932344febcf14c05e215d741ea3136a8179
GabrielRMCorrea <35786887+GabrielRMCorrea@users.noreply.github.com>
2025-03-19 21:03:31 -0300

    Formated code to gdscript best pratices

--- pie_chart.gd
+++ pie_chart.gd
@@ -1,109 +1,190 @@
-class_name Pie_Chart
+class_name PieChart
 extends Control
 
 enum ColorScaleOptions {
-	Preset_1,
-	Preset_2,
-	Gradient_,
-	Custom,
+	PRESET_1,
+	PRESET_2,
+	GRADIENT_,
+	CUSTOM,
 }
-enum LegendStyleOptions {
-	DirectLine,
-	DirectLabel,
-	SeparatedLabel
-}
-@export var Elements : Dictionary[String,float]
-@export var Title : String
-@export var LegendStyle : LegendStyleOptions = LegendStyleOptions.DirectLine
-@export var DoughnutShape : bool = true
-@export var CenterText : String #if DoughnutShape
-@export var CenterProportion : float = 60 #if DoughnutShape
-@export var SeparationLines : bool = false
-@export var ColorScale : ColorScaleOptions = ColorScaleOptions.Preset_1
-@export var CustomScale : Array[Color] #if ColorScale = Custom
-@export var ScaleGradient : Gradient #if ColorScale = Gradient
-@export_group("Style Properties")
-@export var TitleTextColor : Color = Color.WHITE
-@export var TitleFontSize : int = 16
-@export var ElementsTextColor : Color = Color.WHITE
-@export var ElementsFontSize : int = 16
-@export var CenterColor : Color = Color.WHITE
-@export var CenterTextColor : Color = Color.BLACK
-@export var CenterTextFontSize : int = 16
-@export var LineColor : Color = Color.WHITE
 
-func draw_circle_arc_poly(center, radius, angle_from, angle_to, color):
-	var nb_points := 32
-	var points_arc := PackedVector2Array()
+enum LegendStyleOptions {
+	DIRECT_LINE,
+	DIRECT_LABEL,
+	SEPARATED_LABEL,
+}
+
+@export var elements: Dictionary[String, float]
+@export var title: String
+@export var legend_style: LegendStyleOptions = LegendStyleOptions.DIRECT_LINE
+@export var doughnut_shape: bool = true
+@export var center_text: String  # if doughnut_shape
+@export var center_proportion: float = 60.0  # if doughnut_shape
+@export var separation_lines: bool = false
+@export var color_scale: ColorScaleOptions = ColorScaleOptions.PRESET_1
+@export var custom_scale: Array[Color]  # if color_scale == CUSTOM
+@export var scale_gradient: Gradient  # if color_scale == GRADIENT_
+
+@export_group("Style Properties")
+@export var title_text_color: Color = Color.WHITE
+@export var title_font_size: int = 16
+@export var elements_text_color: Color = Color.WHITE
+@export var elements_font_size: int = 16
+@export var center_color: Color = Color.WHITE
+@export var center_text_color: Color = Color.BLACK
+@export var center_text_font_size: int = 16
+@export var line_color: Color = Color.WHITE
+
+func draw_circle_arc_poly(center: Vector2, radius: float, angle_from: float, angle_to: float, color: Color) -> void:
+	var nb_points: int = 32
+	var points_arc: PackedVector2Array = PackedVector2Array()
 	points_arc.push_back(center)
+	
 	for i in range(nb_points + 1):
-		var angle_point = deg_to_rad(angle_from + i * (angle_to - angle_from) / nb_points )
+		var angle_point: float = deg_to_rad(angle_from + i * (angle_to - angle_from) / nb_points)
 		points_arc.push_back(center + Vector2(cos(angle_point), sin(angle_point)) * radius)
+	
 	draw_colored_polygon(points_arc, color)
 
-func _draw():
-	var radius = min(size.x, size.y)/4 
-	var center = Vector2(size.x/(3 if LegendStyle == LegendStyleOptions.SeparatedLabel else 2), size.y/2)
-	var previousAngle : float = 0
-	var counter : int = 0
-	var valuesSize : int =Elements.values().filter(func(number): return number > 0).size()
-	var total : float = Elements.values().reduce(func sum(accum, number): return accum + number) if Elements.values() else 0
-	var separationLinesParameters : Array
-	for i in Elements:
-		if Elements[i] > 0.0:
-			#chosing color
-			var color : Color 
-			match ColorScale :
-				ColorScaleOptions.Preset_1:
-					@warning_ignore("integer_division")
-					color =  Color.from_hsv(1.0/(valuesSize+1) * counter/2 if counter%2 == 0 else 1.0/(valuesSize+1) * (valuesSize  - counter/2) ,0.6 if counter%4<2 else 0.8 ,0.9)
-				ColorScaleOptions.Preset_2:
-					color = Color.from_ok_hsl(1.0/(valuesSize) * counter ,1,0.8  if counter%2 else 0.5)
-				ColorScaleOptions.Gradient_:
-					color = ScaleGradient.sample(float(counter)/(valuesSize-1))
-				ColorScaleOptions.Custom:
-					color = CustomScale[counter%CustomScale.size()]
-			counter += 1
-			#drawing on the screen
-			var percentage: float = Elements[i]/(total/100)
-			var currentAngle:float = 360 * (percentage/100)
-			var angle := deg_to_rad(currentAngle + previousAngle)
-			var mid_angle = angle - deg_to_rad(currentAngle / 2)
-			var anglePoint : Vector2 =  Vector2( cos(mid_angle), sin(mid_angle) ) * radius
-			var text = i + (" - " if LegendStyle == LegendStyleOptions.SeparatedLabel else "\n") + str(snappedf(percentage,0.01)).pad_decimals(2) + "%"
-			var label_size = ThemeDB.fallback_font.get_multiline_string_size(text,HORIZONTAL_ALIGNMENT_CENTER)
-			var label_center =Vector2(label_size.x/2, label_size.y/8)
-
-			var labelPosition : Vector2
-			match LegendStyle:
-				
-				LegendStyleOptions.DirectLine:
-					labelPosition = center - label_center + anglePoint * 1.5
-					draw_line(anglePoint * 1.05 +center, anglePoint * 1.2 + center,LineColor, 2, true)
-				
-				LegendStyleOptions.DirectLabel:
-					var dir_sign = Vector2( sign(cos(mid_angle)), sign(sin(mid_angle)) )
-					var offset = Vector2(dir_sign.x * label_size.x / 2,	dir_sign.y * label_size.y / 2 )
-					labelPosition =   center- label_center + anglePoint *1.05  +offset 
-				
-				LegendStyleOptions.SeparatedLabel:
-					labelPosition.x = size.x - label_size.x -(radius/5) - label_size.y
-					labelPosition.y = label_size.y + (label_size.y +5)*counter +(radius/5)
-					draw_rect(Rect2(Vector2(size.x -(radius/7) - label_size.y, (label_size.y +5)*counter +(radius/5) +5),Vector2.ONE * label_size.y), color )
-
-			draw_multiline_string(ThemeDB.fallback_font, labelPosition, text ,HORIZONTAL_ALIGNMENT_RIGHT if LegendStyle == LegendStyleOptions.SeparatedLabel else HORIZONTAL_ALIGNMENT_CENTER, label_size.x,ElementsFontSize,-1,ElementsTextColor)
-			draw_circle_arc_poly( center, radius,previousAngle ,previousAngle + currentAngle , color)
-			separationLinesParameters.append([center, center +Vector2(cos(angle), sin(angle)) * radius, Color.WHITE, 2, true])
-			previousAngle += currentAngle
+func _draw() -> void:
+	var radius: float = min(size.x, size.y) / 4.0
+	var center: Vector2 = Vector2(
+		size.x / (3.0 if legend_style == LegendStyleOptions.SEPARATED_LABEL else 2.0),
+		size.y / 2.0
+	)
+	var previous_angle: float = 0.0
+	var counter: int = 0
+	var values: Array = elements.values().filter(func(number): return number > 0)
+	var values_size: int = values.size()
+	var total: float = values.reduce(func sum(accum, number): return accum + number) if values else 0.0
+	var separation_lines_parameters: Array = []
 	
-	draw_multiline_string(ThemeDB.fallback_font, Vector2(35,35), Title, HORIZONTAL_ALIGNMENT_CENTER,-1,TitleFontSize,-1,TitleTextColor)
+	for key: String in elements:
+		if elements[key] <= 0.0:
+			continue
+		
+		# Choosing color
+		var color: Color
+		match color_scale:
+			ColorScaleOptions.PRESET_1:
+				@warning_ignore("integer_division")
+				color = Color.from_hsv(
+					1.0 / (values_size + 1) * counter / 2 if counter % 2 == 0 
+						else 1.0 / (values_size + 1) * (values_size - counter / 2),
+					0.6 if counter % 4 < 2 else 0.8,
+					0.9
+				)
+			
+			ColorScaleOptions.PRESET_2:
+				color = Color.from_ok_hsl(
+					1.0 / values_size * counter,
+					1.0,
+					0.8 if counter % 2 else 0.5
+				)
+			
+			ColorScaleOptions.GRADIENT_:
+				color = scale_gradient.sample(float(counter) / (values_size - 1))
+			
+			ColorScaleOptions.CUSTOM:
+				color = custom_scale[counter % custom_scale.size()]
+		
+		counter += 1
+		
+		# Drawing elements
+		var percentage: float = elements[key] / (total / 100.0)
+		var current_angle: float = 360.0 * (percentage / 100.0)
+		var angle: float = deg_to_rad(current_angle + previous_angle)
+		var mid_angle: float = angle - deg_to_rad(current_angle / 2.0)
+		var angle_point: Vector2 = Vector2(cos(mid_angle), sin(mid_angle)) * radius
+		var text: String = key + (" - " if legend_style == LegendStyleOptions.SEPARATED_LABEL else "\n") + str(snappedf(percentage,0.01)).pad_decimals(2) + "%"
+		
+		var label_size: Vector2 = ThemeDB.fallback_font.get_multiline_string_size(
+			text, HORIZONTAL_ALIGNMENT_CENTER
+		)
+		var label_center: Vector2 = Vector2(label_size.x / 2.0, label_size.y / 8.0)
+		var label_position: Vector2
+		
+		match legend_style:
+			LegendStyleOptions.DIRECT_LINE:
+				label_position = center - label_center + angle_point * 1.5
+				draw_line(
+					angle_point * 1.05 + center,
+					angle_point * 1.2 + center,
+					line_color,
+					2.0,
+					true
+				)
+			
+			LegendStyleOptions.DIRECT_LABEL:
+				var dir_sign: Vector2 = Vector2(sign(cos(mid_angle)), sign(sin(mid_angle)))
+				var offset: Vector2 = Vector2(
+					dir_sign.x * label_size.x / 2.0,
+					dir_sign.y * label_size.y / 2.0
+				)
+				label_position = center - label_center + angle_point * 1.05 + offset
+			
+			LegendStyleOptions.SEPARATED_LABEL:
+				label_position.x = size.x - label_size.x - (radius / 5.0) - label_size.y
+				label_position.y = label_size.y + (label_size.y + 5.0) * counter + (radius / 5.0)
+				draw_rect(
+					Rect2(
+						Vector2(
+							size.x - (radius / 7.0) - label_size.y,
+							(label_size.y + 5.0) * counter + (radius / 5.0) + 5.0
+						),
+						Vector2.ONE * label_size.y
+					),
+					color
+				)
+		
+		draw_multiline_string(
+			ThemeDB.fallback_font,
+			label_position,
+			text,
+			HORIZONTAL_ALIGNMENT_RIGHT if legend_style == LegendStyleOptions.SEPARATED_LABEL \
+				else HORIZONTAL_ALIGNMENT_CENTER,
+			label_size.x,
+			elements_font_size,
+			-1,
+			elements_text_color
+		)
+		
+		draw_circle_arc_poly(center, radius, previous_angle, previous_angle + current_angle, color)
+		separation_lines_parameters.append([
+			center,
+			center + Vector2(cos(angle), sin(angle)) * radius,
+			Color.WHITE,
+			2.0,
+			true
+		])
+		previous_angle += current_angle
 	
-	if SeparationLines:
-		for i in separationLinesParameters:
-			draw_line.callv(i)
-
-	if DoughnutShape:
-		draw_circle(center, radius*CenterProportion/100.0, CenterColor)
-		var label_size = ThemeDB.fallback_font.get_multiline_string_size(Title)
-		var font = ThemeDB.fallback_font
-		draw_multiline_string(font, center - Vector2(label_size.x/2, -label_size.y/4), CenterText ,HORIZONTAL_ALIGNMENT_CENTER, label_size.x,CenterTextFontSize,-1, CenterTextColor)
+	# Draw title
+	draw_multiline_string(
+		ThemeDB.fallback_font,
+		Vector2(35.0, 35.0),
+		title,
+		HORIZONTAL_ALIGNMENT_CENTER,
+		-1.0,
+		title_font_size,
+		-1,
+		title_text_color
+	)
+	
+	if separation_lines:
+		for params in separation_lines_parameters:
+			draw_line.callv(params)
+	
+	if doughnut_shape:
+		draw_circle(center, radius * center_proportion / 100.0, center_color)
+		var label_size: Vector2 = ThemeDB.fallback_font.get_multiline_string_size(center_text)
+		draw_multiline_string(
+			ThemeDB.fallback_font,
+			center - Vector2(label_size.x / 2.0, -label_size.y / 4.0),
+			center_text,
+			HORIZONTAL_ALIGNMENT_CENTER,
+			label_size.x,
+			center_text_font_size,
+			-1,
+			center_text_color
+		)
