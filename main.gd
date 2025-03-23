788a8665e561a482c9e9e44f2e76d99c799af458
GabrielRMCorrea <35786887+GabrielRMCorrea@users.noreply.github.com>
2025-03-23 13:33:06 -0300

    removing useless files

--- main.gd
+++
@@ -1,5 +0,0 @@
-extends GridContainer
-@onready var teste :PieChart = $Control/PieChart
-
-func _ready() -> void:
-	teste.elements= {"Unity" : 3324, "Godot" : 2838, "GameMaker" : 389, "Unreal" : 383, "Other" : 777}
